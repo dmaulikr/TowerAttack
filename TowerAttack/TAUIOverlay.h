233892d3d526a084aea7df26da4fa89421495520
Ethan Hardy <ehardy45@gmail.com>
2014-07-09 15:56:58 -0400

    More TD attributes added; gold partially integrated

---
+++ TowerAttack/TAUIOverlay.h
@@ -0,0 +1,13 @@
+//
+//  TAUIOverlay.h
+//  TowerAttack
+//
+//  Created by Ethan Hardy on 2014-07-09.
+//  Copyright (c) 2014 Ethan Hardy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAUIOverlay : UIView
+
+@end
---
+++ TowerAttack/TAUIOverlay.m
@@ -0,0 +1,29 @@
+//
+//  TAUIOverlay.m
+//  TowerAttack
+//
+//  Created by Ethan Hardy on 2014-07-09.
+//  Copyright (c) 2014 Ethan Hardy. All rights reserved.
+//
+
+#import "TAUIOverlay.h"
+
+@implementation TAUIOverlay
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
